151f9a742f0809d1e6fa807918e0f11cc55c0d2a
Janson A <jansonja386@gmail.com>
2026-07-29 22:08:18 +0530

    Change calendar reminder to 1 hour only

--- src/MainInvitation.jsx
+++ src/MainInvitation.jsx
@@ -299,11 +299,6 @@ SUMMARY:10th Batch Reunion
 DESCRIPTION:Let's come together and celebrate!
 LOCATION:St. Francis Xavier's High School, Parisakkal
 BEGIN:VALARM
-TRIGGER:-P1D
-ACTION:DISPLAY
-DESCRIPTION:Reminder: 10th Batch Reunion tomorrow!
-END:VALARM
-BEGIN:VALARM
 TRIGGER:-PT1H
 ACTION:DISPLAY
 DESCRIPTION:Reminder: 10th Batch Reunion in 1 hour!
